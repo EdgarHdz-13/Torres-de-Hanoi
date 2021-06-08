ca39bc6f8710f99ffcc9cb11e3f04c81060aabf5
EdgarHdz-13 <85575704+EdgarHdz-13@users.noreply.github.com>
2021-06-08 12:38:23 -0500

    Contador número torres de Hanoi
    implemente un contador de las 3 torres para llevar un registro de cuantos aros tiene actualmente guardado en los registros a1 a2 a3, también inicie el codigo principal para las torres de Hanoi

--- Torre de Hanoi.asm
+++ Torre de Hanoi.asm
@@ -4,8 +4,8 @@
 		
 		addi	$s0,$zero,100				#Tamaño de la torre de hanoi
 		#Construcción de la torre de Hanoi
+		
 		#La dirección de las 3 torres de Hanoi como mi intención es hacerlo vertical se podria decir que desperdicio memoria pero es más visual e integro la memoria alineada 2 x 1
-				
 		addi	$s1,$zero,0				#origen
 		addi	$s2,$zero,0				#auxiliar
 		addi	$s3,$zero,0				#destino
@@ -25,66 +25,92 @@
 		add	$s2,$s2,$t0				
 		add	$s3,$s3,$t0				
 		
-		#Empiezo a guardar mis aros (números)
+		#Tamaño de las torres de Hanoi
+		add	$a1,$zero,0	
+		add	$a2,$zero,0
+		add	$a3,$zero,0
+		
+main:		#Empiezo a guardar mis aros (números) desde lo más alto de la memoria hasta lo más bajo para que quede vista de manera vertical ascendente
 		add	$t3,$zero,$s0
 ciclo_1:	
 		beq	$t3,$zero,termina_ciclo_1
 		
 		#preparo la función apilar
 		add	$t0,$zero,$t3
-		addi	$t1,$zero,1	
+		addi	$t1,$zero,1
 		add	$t2,$zero,$s0
 		jal	apilar
 		addi	$t3,$t3,-1
 		j	ciclo_1		
 termina_ciclo_1:
-
-		addi	$t0,$zero,1
-		jal	desapilar
+		add	$t0,$zero,$s0
+		addi	$t1,$zero,1
+		addi	$t2,$zero,2
+		addi	$t3,$zero,3
+		
 								
 		j exit
 		
+Hanoi:		#t0 = n, t1 = origen, t2 = auxiliar, t3 = destino, borra t9
+		addi	$t9,$zero,1
+		bne	$t0,$t4,else
+		#Si llega aqui ya empezo a apilar y desapilar
+		add	$t0,$t1,$zero
+		jal	desapilar
+		jal 	apilar
+		jr	$ra		
+else:
+
 		
+				
+						
+								
+										
+														
 		
-apilar:		#(t0 = Dato, t1 = Torre de Hanoi  1, 2 ,3  t2 = cantidad)
-		addi	$t2,$zero,1
-		bne	$t1,$t2,case_2_a
+apilar:		#(t0 = Dato, t1 = Torre de Hanoi  1, 2 ,3  t9 = borra)
+		addi	$t9,$zero,1
+		bne	$t1,$t9,case_2_a
 		addi	$s1,$s1,-0x20				#Antes lo habia puesto después, pero para contar el 0 este addi lo puse antes de sw
 		sw	$t0,0($s1)
+		addi	$a1,$a1,1
 		jr	$ra
 		
-case_2_a:	addi	$t2,$zero,2
-		bne	$t1,$t2,case_3_a
+case_2_a:	addi	$t9,$zero,2
+		bne	$t1,$t9,case_3_a
 		addi	$s2,$s2,-0x20
 		sw	$t0,0($s2)
+		addi	$a2,$a2,1
 		jr	$ra
 		
 case_3_a:	#Podria poner un default, pero confio que no pondré mal esto en el futuro 
 		addi	$s3,$s3,-0x20
 		sw	$t0,0($s3)
-		
+		addi	$a3,$a3,1
 		jr	$ra
 		
 desapilar:	#(t0 = Torre de Hanoi  1, 2 ,3  Borra t1, Y se guarda el dato en t0 lo cual ayudará después)
-		addi	$t1,$zero,1
-		bne	$t0,$t1,case_2_d
+		addi	$t9,$zero,1
+		bne	$t0,$t9,case_2_d
 		lw	$t0,0($s1)
 		sw	$zero,0($s1)
 		addi	$s1,$s1,0x0020
+		addi	$a1,$a1,-1
 		jr	$ra
 		
-case_2_d:	addi	$t1,$zero,2
-		bne	$t0,$t1,case_3_d
-		
+case_2_d:	addi	$t9,$zero,2
+		bne	$t0,$t9,case_3_d
 		lw	$t0,0($s2)
 		sw	$zero,0($s2)
 		addi	$s2,$s2,0x0020
+		addi	$a2,$a2,-1
 		jr	$ra
 		
 case_3_d:		#Podria poner un default, pero confio que no pondré mal esto en el futuro 
 		lw	$t0,0($s0)
 		sw	$zero,0($s0)
 		addi	$s3,$s3,0x0020
+		addi	$a3,$a3,-1
 		jr	$ra
 		
 exit:
