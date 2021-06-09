d718bdeaa2f46b37c96b1bf3e12c6e97f86881c3
EdgarHdz-13 <85575704+EdgarHdz-13@users.noreply.github.com>
2021-06-09 15:15:00 -0500

    Más comentarios
    Unos cuantos comentarios más para que quede bien explicado

--- Torre de Hanoi.asm
+++ Torre de Hanoi.asm
@@ -4,10 +4,9 @@
 .data
 .eqv	direccion_inicial 0x1001  
 .text
-		
+		#Datos inicial
 		addi	$s0,$zero,5				#Tamaño de la torre de hanoi
 		#Construcción de la torre de Hanoi
-		
 		#La dirección de las 3 torres de Hanoi como mi intención es hacerlo vertical se podria decir que desperdicio memoria pero es más visual e integro la memoria alineada 2 x 1
 		addi	$s1,$zero,0				#dirección origen
 		addi	$s2,$zero,0				#dirección auxiliar
@@ -35,9 +34,9 @@
 		add	$a3,$zero,0				#tamaño destino
 		
 main:		#Empiezo a guardar mis aros (números) desde lo más alto de la memoria hasta lo más bajo para que quede vista de manera vertical ascendente
-
 		add	$t0,$zero,$s0	
-		addi	$t3,$zero,1			
+		addi	$t3,$zero,1				#como no hay nada que cambia $t3 optimizo la cantidad de codigo que hago
+		
 ciclo_1:	#preparo la función apilar en t0 pongo el dato que quiero poner y lo voy comparando con el tamaño deseado
 		jal	apilar
 		addi	$t0,$t0,-1
@@ -83,33 +82,35 @@ else:
 		#Bajo el contador de cuantos aros tiene nuestra torre de destino
 		addi	$t4,$t4,-1
 		jal	Hanoi
+		#Empiezo a cargar los datos tal cual están en nuestro stack guardados, los movimientos ya se hicieron previamente
 		lw	$t4,4($sp)
 		lw	$t1,8($sp)
 		lw	$t2,12($sp)
 		lw	$t3,16($sp)
 		lw	$ra,20($sp)
 		addi 	$sp,$sp,20
-		
+		#como ya habia hecho previamente guardo la dirección de $ra y desapilo la torre "origen" y la pongo en "destino"
 		addi	$sp,$sp,-4
 		sw	$ra,4($sp)
 		jal	desapilar
 		jal 	apilar
 		lw	$ra,4($sp)
 		addi	$sp,$sp,4
-		
+		#Guardo todos los datos necesarios para hacer una recursión, estos son t4=Tamaño, t1=Origen, t2=auxiliar, t3=destino, ra = return address
 		addi 	$sp,$sp,-20
 		sw	$t4,4($sp)
 		sw	$t1,8($sp)
 		sw	$t2,12($sp)
 		sw	$t3,16($sp)
 		sw	$ra,20($sp)
-		
+		#Ahora hago el cambio entre las entradas origen y destino, se puede decir que se guarda el movimiento de a -> d
 		lw	$t2,8($sp)
 		lw	$t1,12($sp)
 		lw	$t3,16($sp)
-		
+		#aqui también necesito que bajar el tamaño temporal de la torre origen
 		addi	$t4,$t4,-1
 		jal	Hanoi
+		#Vuelvo a cargar los datos tal cual están ya que en teoria después del $ra se continua donde se dejo la función Hanoi ya sea recursivo o hacia donde comenzo
 		lw	$t4,4($sp)
 		lw	$t1,8($sp)
 		lw	$t2,12($sp)
@@ -126,6 +127,7 @@ else:
 apilar:		#(t0 = input Dato, t3 = Torre de Hanoi  1, 2 ,3  t9 = borra)
 		addi	$t9,$zero,1
 		bne	$t3,$t9,case_2_a
+		#Aqui si se escogio la torre 1, apilo en donde me apunta $s1(Dirección torre de hanoi 1) el dato $t0
 		addi	$s1,$s1,-0x20				#Antes lo habia puesto después, pero para contar el 0 este addi lo puse antes de sw
 		sw	$t0,0($s1)
 		addi	$a1,$a1,1
@@ -133,12 +135,13 @@ apilar:		#(t0 = input Dato, t3 = Torre de Hanoi  1, 2 ,3  t9 = borra)
 		
 case_2_a:	addi	$t9,$zero,2
 		bne	$t3,$t9,case_3_a
+		#Aqui si se escogio la torre 2, apilo en donde me apunta $s2(Dirección torre de hanoi 2) el dato $t0
 		addi	$s2,$s2,-0x20
 		sw	$t0,0($s2)
 		addi	$a2,$a2,1
 		jr	$ra
 		
-case_3_a:	#Podria poner un default, pero confio que no pondré mal esto en el futuro 
+case_3_a:	#Podria poner un default, pero confio que no pondré mal esto en el futuro y apila donde apunta $s3(Dirección torre de Hanoi 3) el dato $t0
 		addi	$s3,$s3,-0x20
 		sw	$t0,0($s3)
 		addi	$a3,$a3,1
@@ -147,6 +150,7 @@ case_3_a:	#Podria poner un default, pero confio que no pondré mal esto en el fut
 desapilar:	#(t0 = Output dato, t1 = Torre de Hanoi  1, 2 ,3  Borra t9)
 		addi	$t9,$zero,1
 		bne	$t1,$t9,case_2_d
+		#Aqui si se escogio la torre 1, desapilo en donde me apunta $s1(Dirección torre de hanoi 1) y guardo el dato en $t0
 		lw	$t0,0($s1)
 		sw	$zero,0($s1)
 		addi	$s1,$s1,0x0020
@@ -155,13 +159,14 @@ desapilar:	#(t0 = Output dato, t1 = Torre de Hanoi  1, 2 ,3  Borra t9)
 		
 case_2_d:	addi	$t9,$zero,2
 		bne	$t1,$t9,case_3_d
+		#Aqui si se escogio la torre 2, desapilo en donde me apunta $s2(Dirección torre de hanoi 2) y guardo el dato en $t0
 		lw	$t0,0($s2)
 		sw	$zero,0($s2)
 		addi	$s2,$s2,0x0020
 		addi	$a2,$a2,-1
 		jr	$ra
 		
-case_3_d:	#Podria poner un default, pero confio que no pondré mal esto en el futuro 
+case_3_d:	#Podria poner un default, pero confio que no pondré mal esto en el futuro y desapila donde apunta $s3(Dirección torre de Hanoi 3) y guarda el dato en $t0
 		lw	$t0,0($s3)
 		sw	$zero,0($s3)
 		addi	$s3,$s3,0x0020
