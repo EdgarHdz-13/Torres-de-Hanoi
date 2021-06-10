4da6108eefa6e890ad65b27393967a102e3f73b4
EdgarHdz-13 <85575704+EdgarHdz-13@users.noreply.github.com>
2021-06-10 18:27:11 -0500

    Corrección de Registros
    Corrección de algunos comentarios y registros

--- Torre de Hanoi.asm
+++ Torre de Hanoi.asm
@@ -5,9 +5,9 @@
 .eqv	direccion_inicial 0x1001  
 .text
 		#Datos inicial
-		addi	$s0,$zero,4				#Tamaño de la torre de hanoi
+		addi	$s0,$zero,8				#Tamaño de la torre de hanoi
 		#Construcción de la torre de Hanoi
-		#La dirección de las 3 torres de Hanoi como mi intención es hacerlo vertical se podria decir que desperdicio memoria pero es más visual e integro la memoria alineada 2 x 1
+		#La dirección de las 3 torres de Hanoi como mi intención es hacerlo vertical se podria decir que desperdicio memoria pero es más visual
 		addi	$s1,$zero,0				#dirección origen
 		addi	$s2,$zero,0				#dirección auxiliar
 		addi	$s3,$zero,0				#dirección destino
@@ -52,8 +52,8 @@ ciclo_1:	#preparo la función apilar en t0 pongo el dato que quiero poner y lo vo
 		j exit
 		
 Hanoi:		#t4 = n, t1 = origen, t2 = auxiliar, t3 = destino, borra t9
-		addi	$t9,$zero,1
-		bne	$t4,$t9,else
+		addi	$at,$zero,1
+		bne	$t4,$at,else
 		
 		#Si llega aqui significa que ya llegue a que este t4 = 1 lo que va a hacer que empiece a apilar y desapilar torres  ahora aqui hay algo interesante y es que como se a donde 
 		#se dirigen los datos, y esto va que antes de llegar aqui se debieron de haber cargado con anterioridad, normalmente hago que vaya de origen hacia destino pero esto no siempre
@@ -75,7 +75,7 @@ else:
 		sw	$t2,12($sp)
 		sw	$t3,16($sp)
 		sw	$ra,20($sp)
-		#Cambio el auxiliar y el destino para seguir el formato del codigo recursivo Hanoi(n-1,o,d,a) -> Hanoi(n-1,o,a,d) se podria decir que guardo el movimiento o -> d
+		#Cambio el auxiliar y el destino para seguir el formato del codigo recursivo Hanoi(n-1,o,d,a) -> Hanoi(n-1,o,a,d) se podria decir que guardo el movimiento o -> a
 		lw	$t3,12($sp)
 		lw	$t2,16($sp)
 		#Bajo el contador de cuantos aros tiene nuestra torre de destino
@@ -102,7 +102,7 @@ else:
 		sw	$t2,12($sp)
 		sw	$t3,16($sp)
 		sw	$ra,20($sp)
-		#Ahora hago el cambio entre las entradas origen y destino, se puede decir que se guarda el movimiento de a -> d
+		#Ahora hago el cambio entre las entradas origen y auxiliar o-a, se puede decir que se guarda el movimiento de a -> d
 		lw	$t2,8($sp)
 		lw	$t1,12($sp)
 		#aqui también necesito que bajar el tamaño temporal de la torre origen
@@ -116,23 +116,19 @@ else:
 		lw	$ra,20($sp)
 		addi 	$sp,$sp,20
 		
-		jr	$ra
-						
-								
-										
-														
+		jr	$ra													
 		
 apilar:		#(t0 = input Dato, t3 = Torre de Hanoi  1, 2 ,3  t9 = borra)
-		addi	$t9,$zero,1
-		bne	$t3,$t9,case_2_a
+		addi	$at,$zero,1
+		bne	$t3,$at,case_2_a
 		#Aqui si se escogio la torre 1, apilo en donde me apunta $s1(Dirección torre de hanoi 1) el dato $t0
 		addi	$s1,$s1,-0x20				#Antes lo habia puesto después, pero para contar el 0 este addi lo puse antes de sw
 		sw	$t0,0($s1)
 		addi	$a1,$a1,1
 		jr	$ra
 		
-case_2_a:	addi	$t9,$zero,2
-		bne	$t3,$t9,case_3_a
+case_2_a:	addi	$at,$zero,2
+		bne	$t3,$at,case_3_a
 		#Aqui si se escogio la torre 2, apilo en donde me apunta $s2(Dirección torre de hanoi 2) el dato $t0
 		addi	$s2,$s2,-0x20
 		sw	$t0,0($s2)
@@ -146,8 +142,8 @@ case_3_a:	#Podria poner un default, pero confio que no pondré mal esto en el fut
 		jr	$ra
 		
 desapilar:	#(t0 = Output dato, t1 = Torre de Hanoi  1, 2 ,3  Borra t9)
-		addi	$t9,$zero,1
-		bne	$t1,$t9,case_2_d
+		addi	$at,$zero,1
+		bne	$t1,$at,case_2_d
 		#Aqui si se escogio la torre 1, desapilo en donde me apunta $s1(Dirección torre de hanoi 1) y guardo el dato en $t0
 		lw	$t0,0($s1)
 		sw	$zero,0($s1)
@@ -155,8 +151,8 @@ desapilar:	#(t0 = Output dato, t1 = Torre de Hanoi  1, 2 ,3  Borra t9)
 		addi	$a1,$a1,-1
 		jr	$ra
 		
-case_2_d:	addi	$t9,$zero,2
-		bne	$t1,$t9,case_3_d
+case_2_d:	addi	$at,$zero,2
+		bne	$t1,$at,case_3_d
 		#Aqui si se escogio la torre 2, desapilo en donde me apunta $s2(Dirección torre de hanoi 2) y guardo el dato en $t0
 		lw	$t0,0($s2)
 		sw	$zero,0($s2)
